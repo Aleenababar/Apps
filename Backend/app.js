bfea0788e21ee0fdb161ef34942c8000efdcfdaf
Aleenababar <Aleenababar471@gmai.com>
2025-10-07 17:39:55 +0800

    resolve

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // For SPA routing
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/build", "index.html"));
 });
 
 app.use (cors());
